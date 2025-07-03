bdae2f8d4f1bc6c2d2a56d5b0002e0753195857c
xuaanhaor <eji0nkiz@gmail.com>
2025-07-03 21:56:55 +0700

    initial commit

---
+++ src/main/java/com/alphawash/AlphawashApplication.java
@@ -0,0 +1,13 @@
+package com.alphawash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphawashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphawashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alphawash/AlphawashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphawash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphawashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
