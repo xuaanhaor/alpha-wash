a4f00778380da99e33adb6fb83a8fa88de051dce
JAVA_05_NGUYENHUUMINHTAI <tainhmse170053@fpt.edu.vn>
2025-07-08 21:02:14 +0700

    create repo and dao for employee-orderServiceDetail-orderService

--- src/main/java/com/alphawash/repository/ServiceRepository.java
+++ src/main/java/com/alphawash/repository/ServiceRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Integer> {
 
-    // Tìm dịch vụ theo tên chính xác
     Service findByName(String name);
 }
