5eef83f68a93fd7811724b31ad4a99273aab40d2
xuaanhaor <eji0nkiz@gmail.com>
2025-07-10 21:55:37 +0700

    unit test

--- src/test/java/com/alphawash/AlphawashApplicationTests.java
+++ src/test/java/com/alphawash/AlphawashApplicationTests.java
@@ -1,14 +1,31 @@
 package com.alphawash;
 
+import com.alphawash.endpoint.ProductionController;
+import com.alphawash.entity.Production;
+import com.alphawash.response.ProductionResponse;
+import com.alphawash.service.ProductionService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootTest
 class AlphawashApplicationTests {
+    @Mock ProductionService productionService;
+    @InjectMocks
+    ProductionController productionController;
 
     @Test
-    void contextLoads() {
-        Assertions.assertEquals("Hello", "Hello");
+    void testGetAllProductions1() {
+        Mockito.when(productionService.getAllProductions())
+                .thenReturn(Collections.emptyList());
+        var resp = productionController.getAllProductions();
+        Assertions.assertEquals(0, resp.getBody().size());
     }
 }
