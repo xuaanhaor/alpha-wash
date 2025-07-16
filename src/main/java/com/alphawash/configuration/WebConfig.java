f3548c4363f16b845789c860ce119662a33b7dc7
JAVA_05_NGUYENHUUMINHTAI <tainhmse170053@fpt.edu.vn>
2025-07-16 22:13:45 +0700

    built

--- src/main/java/com/alphawash/configuration/WebConfig.java
+++ src/main/java/com/alphawash/configuration/WebConfig.java
@@ -1,6 +1,5 @@
 package com.alphawash.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
