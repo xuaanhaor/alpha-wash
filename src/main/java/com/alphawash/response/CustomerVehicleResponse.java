9d152a062cfbddfea6d9dea7cea1bf4a176b4faa
xuaanhaor <eji0nkiz@gmail.com>
2025-07-18 00:38:59 +0700

    add api find customer vehicle by phone

--- src/main/java/com/alphawash/response/CustomerVehicleResponse.java
+++ src/main/java/com/alphawash/response/CustomerVehicleResponse.java
@@ -1,5 +1,7 @@
 package com.alphawash.response;
 
+import com.alphawash.dto.CustomerVehicleDto;
+import java.util.List;
 import java.util.UUID;
 import lombok.*;
 
@@ -9,7 +11,8 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class CustomerVehicleResponse {
-    private String phone;
     private UUID customerId;
     private String customerName;
+    private String phone;
+    List<CustomerVehicleDto> vehicles;
 }
