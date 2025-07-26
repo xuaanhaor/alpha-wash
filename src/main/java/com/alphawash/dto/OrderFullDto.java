4b7d07393bb9c7b990b2d7eed51525ac3741cc37
JAVA_05_NGUYENHUUMINHTAI <tainhmse170053@fpt.edu.vn>
2025-07-27 00:57:51 +0700

    Fix: Multiple order service selection and order detail note persistence and add order code generation

--- src/main/java/com/alphawash/dto/OrderFullDto.java
+++ src/main/java/com/alphawash/dto/OrderFullDto.java
@@ -21,7 +21,6 @@ public class OrderFullDto {
     private BigDecimal tip;
     private BigDecimal vat;
     private BigDecimal discount;
-    private Boolean deleteFlag;
     private BigDecimal totalPrice;
     private String note;
     private CustomerDTO customer;
