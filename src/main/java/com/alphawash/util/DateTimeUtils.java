cbf7c4cbbab4cc24566304c4f854f04fe6299c88
xuaanhaor <eji0nkiz@gmail.com>
2025-07-27 22:13:06 +0700

    add revenue service

--- src/main/java/com/alphawash/util/DateTimeUtils.java
+++ src/main/java/com/alphawash/util/DateTimeUtils.java
@@ -1,9 +1,24 @@
 package com.alphawash.util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtils {
+    public static final String YYYYMMDD = "yyyyMMdd";
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+
     public static LocalDateTime getCurrentDate() {
         return LocalDateTime.now();
+    }
+
+    public static LocalDate convertToLocalDate(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(YYYYMMDD);
+        return LocalDate.parse(date, formatter);
+    }
+
+    public static LocalDate convertDateFormat(String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(YYYYMMDD);
+        return LocalDate.parse(input, formatter);
     }
 }
