abc9e3807e87b1e7e4b2a07609c831716f0fdc06
minhtai-ferb <minhtai7112003@gmail.com>
2025-09-11 20:47:58 +0700

    fix query sql license_plate

--- init-db/01-schema_v4.sql
+++ init-db/01-schema_v4.sql
@@ -136,7 +136,7 @@ CREATE TABLE model
 CREATE TABLE vehicle
 (
     id            UUID      DEFAULT uuid_generate_v4(),
-    license_plate VARCHAR(8) UNIQUE NOT NULL,
+    license_plate VARCHAR(9) UNIQUE NOT NULL,
     customer_id   UUID REFERENCES customer (id),
     brand_code    VARCHAR(20) REFERENCES brands (code),
     model_code    VARCHAR(20) REFERENCES model (code),
